624604a2109fbc6ef47518d6ff017cb789dcf3db
sczembor <43810037+sczembor@users.noreply.github.com>
2023-04-25 18:47:34 +0200

    integration_test

--- contracts/oracle/src/lib.rs
+++ contracts/oracle/src/lib.rs
@@ -1,7 +1,7 @@
 use ed25519_dalek::{PublicKey, Signature, Verifier, PUBLIC_KEY_LENGTH};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LazyOption, UnorderedSet};
-use near_sdk::{env, near_bindgen, require, AccountId, Balance, Gas, PanicOnDefault, Promise};
+use near_sdk::{env, near_bindgen, require, AccountId, Balance, Gas, PanicOnDefault, Promise, PromiseError};
 
 use sbt::*;
 
@@ -162,12 +162,12 @@ impl Contract {
     }
 
     #[private]
-    #[handle_result]
-    pub fn sbt_mint_callback(&mut self, external_id: &Vec<u8>, #[callback_result] last_result: Result<TokenId, near_sdk::PromiseError>) -> Result<TokenId, near_sdk::PromiseError> {
+    pub fn sbt_mint_callback(&mut self, external_id: &Vec<u8>, #[callback_result] last_result: Result<TokenId, PromiseError>) -> Option<TokenId> {
         if last_result.is_ok() {
             self.used_identities.insert(&external_id);
+            return last_result.ok();
         }
-        last_result
+        None
     }
 
     /**********
