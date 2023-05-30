e8ec5646fcb4e0f4652c012844a5acf704ea429a
Robert Zaremba <robert@zaremba.ch>
2023-05-31 01:43:55 +0200

    setup: add build-quick make job

--- contracts/Makefile-common.mk
+++ contracts/Makefile-common.mk
@@ -2,6 +2,11 @@ build:
 	@RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 	@cp ../target/wasm32-unknown-unknown/release/*.wasm ../res/
 
+build-quick:
+	@RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown
+	@cp ../target/wasm32-unknown-unknown/release/*.wasm ../res/
+
+
 build-all:
 	@RUSTFLAGS='-C link-arg=-s' cargo build --all --target wasm32-unknown-unknown --release
 	@cp ../target/wasm32-unknown-unknown/release/*.wasm ../res/
