3447f493c2233a6b5677b0d6108295eead868500
sczembor <stanislaw.czembor@gmail.com>
2023-06-19 11:49:41 +0200

    change expires_at time in revoke to mili seconds

--- contracts/registry/src/lib.rs
+++ contracts/registry/src/lib.rs
@@ -1597,7 +1597,7 @@ mod tests {
         let m2_1 = mk_metadata(2, Some(START + 11));
         let m3_1 = mk_metadata(3, Some(START + 21));
 
-        let current_timestamp = ctx.block_timestamp;
+        let current_timestamp = ctx.block_timestamp / MILI_SECOND; // convert nano to mili seconds
 
         let m1_1_revoked = mk_metadata(1, Some(current_timestamp));
         let m2_1_revoked = mk_metadata(2, Some(current_timestamp));
