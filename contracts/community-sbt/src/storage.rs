59fe0643332eab3ca55a1922f42986d5e0176c5c
Robert Zaremba <robert@zaremba.ch>
2023-07-12 22:10:15 +0200

    Update Community SBT to enable Human gated minting (#51)
    Co-authored-by: sczembor <43810037+sczembor@users.noreply.github.com>

--- contracts/community-sbt/src/storage.rs
+++ contracts/community-sbt/src/storage.rs
@@ -1,5 +1,6 @@
-use near_sdk::borsh::{self, BorshSerialize};
-use near_sdk::BorshStorageKey;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{AccountId, BorshStorageKey};
 
 /// Helper structure for keys of the persistent collections.
 #[derive(BorshSerialize, BorshStorageKey)]
@@ -7,4 +8,13 @@ pub enum StorageKey {
     Admins, // deprecated, required for migration
     ContractMetadata,
     MintingAuthority,
+}
+
+/// Helper structure for keys of the persistent collections.
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
+#[cfg_attr(not(target_arch = "wasm32"), derive(PartialEq, Debug))]
+#[serde(crate = "near_sdk::serde")]
+pub struct ClassMinters {
+    pub requires_iah: bool,
+    pub minters: Vec<AccountId>,
 }
