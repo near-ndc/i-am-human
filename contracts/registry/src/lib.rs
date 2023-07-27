ff0b7d1cdcde19a06f44b56c743266a88c181285
Amit Yadav <amy29981@gmail.com>
2023-07-27 16:55:37 +0530

    Update contracts/registry/src/lib.rs
    Co-authored-by: sczembor <43810037+sczembor@users.noreply.github.com>

--- contracts/registry/src/lib.rs
+++ contracts/registry/src/lib.rs
@@ -2671,8 +2671,6 @@ mod tests {
     #[should_panic(expected = "caller not a human")]
     fn is_human_call_fail() {
         let (mut ctx, mut ctr) = setup(&fractal_mainnet(), 150 * MINT_DEPOSIT);
-        ctx.current_account_id = AccountId::new_unchecked("registry.i-am-human.near".to_string());
-        testing_env!(ctx.clone());
 
         let m1_1 = mk_metadata(1, Some(START));
         ctr.sbt_mint(vec![(alice(), vec![m1_1])]);
