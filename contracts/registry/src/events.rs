66e9ef6c0f604e483ecea764cf72a6e69436a92a
Robert Zaremba <robert@zaremba.ch>
2023-08-16 21:13:13 +0200

    iah events tests

--- contracts/registry/src/events.rs
+++ contracts/registry/src/events.rs
@@ -1,12 +1,11 @@
-use near_sdk::serde::Serialize;
-use near_sdk::{env, AccountId};
+use near_sdk::AccountId;
 use sbt::{EventWrapper, NearEvent};
 
 use crate::storage::AccountFlag;
 
-pub fn emit_iah_account_flag(flag: crate::AccountFlag, account: AccountId) {
+pub fn emit_iah_flag_account(flag: crate::AccountFlag, accounts: Vec<AccountId>) {
     let event = match flag {
-        AccountFlag::Scam => "flag_fake",
+        AccountFlag::Scam => "flag_scam",
         AccountFlag::Trusted => "flag_trusted",
     };
     let e = NearEvent {
@@ -14,8 +13,27 @@ pub fn emit_iah_account_flag(flag: crate::AccountFlag, account: AccountId) {
         version: "1.0.0",
         event: EventWrapper {
             event,
-            data: vec![account], // data is a simple list of accounts to ban
+            data: accounts, // data is a simple list of accounts to ban
         },
     };
     e.emit();
+}
+
+#[cfg(test)]
+mod tests {
+    use near_sdk::test_utils;
+
+    use super::*;
+
+    fn alice() -> AccountId {
+        AccountId::new_unchecked("alice.near".to_string())
+    }
+
+    #[test]
+    fn log_flag_account() {
+        let accounts = vec![alice()];
+        let expected1 = r#"EVENT_JSON:{"standard":"iah","version":"1.0.0","event":"flag_fake","data":["alice.near"]}"#;
+        emit_iah_flag_account(AccountFlag::Scam, accounts);
+        assert_eq!(vec![expected1], test_utils::get_logs());
+    }
 }
