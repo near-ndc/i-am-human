23daae72278fbea86526f10d2def3eec6909b33a
Robert Zaremba <robert@zaremba.ch>
2023-08-17 10:44:25 +0200

    feat(registry): flagged accounts support (#69)
    Co-authored-by: sczembor <43810037+sczembor@users.noreply.github.com>
    Co-authored-by: Amit Yadav <amy29981@gmail.com>

--- contracts/registry/src/migrate.rs
+++ contracts/registry/src/migrate.rs
@@ -1,37 +1,32 @@
 use crate::*;
 
+// registry/v1.3.0
 #[derive(BorshDeserialize, PanicOnDefault)]
 pub struct OldState {
-    /// Registry admin, expected to be a DAO.
     pub authority: AccountId,
-
-    /// registry of approved SBT contracts to issue tokens
     pub sbt_issuers: UnorderedMap<AccountId, IssuerId>,
     pub issuer_id_map: LookupMap<IssuerId, AccountId>, // reverse index
-    /// registry of blacklisted accounts by issuer
-    pub banlist: UnorderedSet<AccountId>,
-
+    pub(crate) banlist: UnorderedSet<AccountId>,
+    pub(crate) ongoing_soul_tx: LookupMap<AccountId, IssuerTokenId>,
     pub(crate) supply_by_owner: LookupMap<(AccountId, IssuerId), u64>,
     pub(crate) supply_by_class: LookupMap<(IssuerId, ClassId), u64>,
     pub(crate) supply_by_issuer: LookupMap<IssuerId, u64>,
-    /// maps user account to list of token source info
     pub(crate) balances: TreeMap<BalanceKey, TokenId>,
-    /// maps SBT contract -> map of tokens
     pub(crate) issuer_tokens: LookupMap<IssuerTokenId, TokenData>,
-    /// map of SBT contract -> next available token_id
     pub(crate) next_token_ids: LookupMap<IssuerId, TokenId>,
     pub(crate) next_issuer_id: IssuerId,
-    pub(crate) ongoing_soul_tx: LookupMap<AccountId, IssuerTokenId>,
+    pub(crate) iah_sbts: (AccountId, Vec<ClassId>),
 }
 
 #[near_bindgen]
 impl Contract {
     #[private]
     #[init(ignore_state)]
-    pub fn migrate(iah_issuer: AccountId, iah_classes: Vec<ClassId>) -> Self {
-        // retrieve the current state from the contract
+    pub fn migrate(authorized_flaggers: Vec<AccountId>) -> Self {
         let old_state: OldState = env::state_read().expect("failed");
-        // new field in the smart contract : pub(crate) iah_classes: (AccountId, Vec<ClassId>),
+        // new field in the smart contract :
+        // + flagged: LookupMap<AccountId, AccountFlag>
+        // + admins_flagged: LazyOption<Vec<AccountId>>
 
         Self {
             authority: old_state.authority.clone(),
@@ -46,7 +41,12 @@ impl Contract {
             next_token_ids: old_state.next_token_ids,
             next_issuer_id: old_state.next_issuer_id,
             ongoing_soul_tx: old_state.ongoing_soul_tx,
-            iah_sbts: (iah_issuer.clone(), iah_classes.clone()),
+            iah_sbts: old_state.iah_sbts,
+            flagged: LookupMap::new(StorageKey::Flagged),
+            authorized_flaggers: LazyOption::new(
+                StorageKey::AdminsFlagged,
+                Some(&authorized_flaggers),
+            ),
         }
     }
 }
